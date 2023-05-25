db0cf42e36aaee766725f7336ea32a9bf6a5be26
trieutrinh <trieutrinh2@gmail.com>
2023-05-25 15:42:35 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 80;
 
 app.get('/', (req, res) => {
   res.send('<h1>Express Demo App</h1> <h4>Message: Success</h4> <p>Version 3.1</p>');
